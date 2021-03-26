5af28550c34f265e89a88c7956df9fb9feabfd5d
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2021-03-25 19:04:16 -0700

    Savepoint

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,11 +3,15 @@ const postCssLoader = './postCssLoader'
 module.exports = {
     stories: ['../src/**/*.stories.ts'],
     addons: [
-        '@storybook/addon-actions',
         '@storybook/addon-links',
-        '@storybook/addon-viewport',
-        '@storybook/addon-controls',
         '@storybook/addon-a11y',
         '@storybook/addon-postcss',
+        { name: "@storybook/addon-essentials", options: { docs: false } },
+        // 'storybook-dark-mode',
     ],
+    webpackFinal: async (config) => {
+        const svelteLoader = config.module.rules.find((r) => r.loader && r.loader.includes('svelte-loader'))
+        svelteLoader.options.preprocess = require('svelte-preprocess')()
+        return config
+    },
 }
---
+++ src/elements/component.svelte
@@ -0,0 +1,8 @@
+<script lang='ts'>
+    type Color = 'primary'
+    export const color: Color = 'primary'
+</script>
+
+<span class="rounded-full bg-primary-50 text-primary-700">
+    Hello world
+</span>
