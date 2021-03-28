8ee5f8c3fd02cb2d0ae7b0a60ffc276f0dbc8364
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2021-03-28 09:32:08 -0700

    Savepoint

--- src/typography/text-content/text-content.svelte
+++ src/typography/text-content/text-content.svelte
@@ -13,27 +13,3 @@
 >
     <slot></slot>
 </div>
-
-<style>
-    :global(.prose h1) {
-        @apply h2;
-    }
-    :global(.prose h2) {
-        @apply h3;
-    }
-    :global(.prose h3) {
-        @apply h5;
-    }
-    :global(.prose h4) {
-        @apply h6;
-    }
-    :global(.prose pre) {
-        @apply bg-gray-50 text-gray-900 bg-opacity-70;
-    }
-    :global(.prose a) {
-        @apply link;
-    }
-    :global(.prose img) {
-        @apply rounded-lg;
-    }
-</style>
