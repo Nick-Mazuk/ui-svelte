d962beefd8140f82a20dd7fa3dd832d7d58412f5
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2021-05-04 15:26:49 -0700

    style: remove excess subfooter margin

--- src/components/footer/footer.svelte
+++ src/components/footer/footer.svelte
@@ -3,13 +3,13 @@
 </script>
 
 <footer class="py-12 mt-auto" class:bg-gray-100="{!light}" class:border-t="{light}">
-    <div class="wrapper">
+    <div class="wrapper flex space-y-8">
         <nav class="flex flex-col sm:space-x-6 justify-between sm:flex-row">
             <slot />
         </nav>
         {#if $$slots.subfooter}
             <div
-                class="flex text-gray justify-between md:space-x-6 space-y-4 md:space-y-0 items-center mt-8 md:flex-row flex-col"
+                class="flex text-gray justify-between md:space-x-6 space-y-4 md:space-y-0 items-center md:flex-row flex-col"
             >
                 <slot name="subfooter" />
             </div>
