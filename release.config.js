7a5f1698b2c6d1b4c7fd0c0ac1d3feb10d4deb9a
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2021-05-05 11:26:30 -0700

    ci: migrate semantic-release to custom config

--- release.config.js
+++ release.config.js
@@ -1,29 +1,3 @@
 module.exports = {
-    branches: 'main',
-    plugins: [
-        [
-            '@semantic-release/commit-analyzer',
-            {
-                preset: 'angular',
-                releaseRules: [
-                    { type: 'docs', scope: 'README', release: 'patch' },
-                    { type: 'refactor', release: 'patch' },
-                    { type: 'style', release: 'patch' },
-                    { type: 'bump', release: 'patch' },
-                ],
-                parserOpts: {
-                    noteKeywords: ['BREAKING'],
-                },
-            },
-        ],
-        '@semantic-release/release-notes-generator',
-        '@semantic-release/npm',
-        '@semantic-release/github',
-        [
-            '@semantic-release/changelog',
-            {
-                changelogFile: 'CHANGELOG.md',
-            },
-        ],
-    ],
+    extends: ['@nick-mazuk/semantic-release-config'],
 }
