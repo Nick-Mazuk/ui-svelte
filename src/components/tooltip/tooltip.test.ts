4c0ba6f2688153997cf4f229836f276d330ce173
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2021-05-10 16:34:02 -0700

    test: fix tooltip tests

--- src/components/tooltip/tooltip.test.ts
+++ src/components/tooltip/tooltip.test.ts
@@ -6,15 +6,7 @@ it('functions', () => {
     cy.get('[data-test="tooltip"]').should('be.visible')
     cy.realPress('Tab')
     cy.get('[data-test="tooltip"]').should('not.exist')
-    cy.get('[data-test="tooltip-trigger"]').realHover()
-    cy.get('[data-test="tooltip"]').should('be.visible')
-})
-
-it('is still visible after click', () => {
-    cy.loadStory('Components/Tooltip', 'Custom trigger')
     cy.get('button').realHover()
-    cy.get('[data-test="tooltip"]').should('be.visible')
-    cy.get('button').realClick()
     cy.get('[data-test="tooltip"]').should('be.visible')
 })
 
