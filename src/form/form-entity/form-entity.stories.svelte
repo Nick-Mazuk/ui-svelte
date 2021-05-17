819cd03762147db33706067099cdbb9fea7f1066
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2021-05-17 16:20:24 -0700

    feat: add form entity

---
+++ src/form/form-entity/form-entity.stories.svelte
@@ -0,0 +1,188 @@
+<script lang="ts">
+    import { Meta, Story } from '@storybook/addon-svelte-csf'
+    import Button from '../../elements/button/button.svelte'
+    import { FORM_FEEDBACK } from '../form-feedback'
+    import EmailInput from '../inputs/email-input/email-input.svelte'
+    import TextInput from '../inputs/text-input/text-input.svelte'
+
+    import FormEntity from './form-entity.svelte'
+
+    const meta = {
+        title: 'Form/FormEntity',
+        component: FormEntity,
+        parameters: {
+            docs: {
+                description: {
+                    component: '',
+                },
+            },
+        },
+        argTypes: {
+            title: {
+                control: 'text',
+                defaultValue: 'Form entity',
+            },
+            description: {
+                control: 'text',
+            },
+            descriptionLink: {
+                control: 'object',
+            },
+            primaryAction: {
+                control: 'text',
+                defaultValue: 'Save',
+            },
+            secondaryAction: {
+                control: 'text',
+            },
+            destructiveAction: {
+                control: 'text',
+            },
+            handleSubmit: {
+                description:
+                    'A function to handle the submission. Passed directly to the `Form` component.',
+            },
+            method: {
+                description:
+                    'The method to submit the form. Not needed if using `handleSubmit`. Passed directly to the `Form` component.',
+            },
+            action: {
+                description:
+                    'The url to send the http request to. Not needed if using `handleSubmit`. Passed directly to the `Form` component.',
+            },
+            resetOnSubmit: {
+                control: {
+                    type: 'boolean',
+                },
+                defaultValue: false,
+                description:
+                    'Resets all form inputs to their default values after the form is submitted successfully.',
+            },
+            allowOffline: {
+                control: {
+                    type: 'boolean',
+                },
+                defaultValue: false,
+                description:
+                    'By default, the form will detect if a user is offline and will throw an error. Use this prop to remove this behavior and allow the user to submit the form without internet access.',
+            },
+            success: {
+                control: {
+                    type: 'text',
+                },
+                defaultValue: FORM_FEEDBACK.success.saved,
+            },
+            error: {
+                control: {
+                    type: 'object',
+                },
+                description:
+                    'Error messages to display if the submit fails. Accepts either an object or a string. If `error` is a string, that string will be shown reguardless of the error. If `error` is an object, it replaces any of the default messages. The object can be partial. If any value is set to false, the default error message will be shown instead for that error.',
+                defaultValue: {
+                    default: FORM_FEEDBACK.errors.default,
+                    offline: FORM_FEEDBACK.errors.offline,
+                    403: FORM_FEEDBACK.errors[403],
+                    429: FORM_FEEDBACK.errors[429],
+                    400: FORM_FEEDBACK.errors[400],
+                    500: FORM_FEEDBACK.errors[500],
+                },
+            },
+            progress: {
+                control: {
+                    type: 'range',
+                    min: 0,
+                    max: 1,
+                    step: 0.05,
+                },
+                description: 'For longer tasks like file uploads, show a progress bar.',
+            },
+        },
+    }
+
+</script>
+
+<Meta {...meta} />
+
+<Story name="Default" let:args>
+    <FormEntity
+        {...args}
+        handleSubmit="{() => {
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve(true)
+                }, 500)
+            })
+        }}"
+    >
+        <EmailInput defaultValue="hello@exaple.com" />
+    </FormEntity>
+</Story>
+<Story
+    name="With description"
+    args="{{
+        title: 'Your email',
+        description: 'Enter the email address you want to use to log in.',
+        descriptionLink: { value: 'Learn more', href: '#' },
+    }}"
+    let:args
+>
+    <FormEntity {...args}>
+        <EmailInput defaultValue="hello@exaple.com" />
+    </FormEntity>
+</Story>
+<Story name="Actions" let:args>
+    <div class="grid gap-6">
+        <FormEntity
+            {...args}
+            title="All actions"
+            secondaryAction="Discard"
+            destructiveAction="Delete"
+        >
+            <EmailInput defaultValue="hello@exaple.com" />
+        </FormEntity>
+        <FormEntity
+            {...args}
+            title="No actions"
+            description="Set the primary action to an empty string to remove it"
+            primaryAction=""
+        >
+            <EmailInput defaultValue="hello@exaple.com" />
+        </FormEntity>
+        <FormEntity
+            {...args}
+            title="Destructive entity"
+            description="Set the primary action to an empty string to remove it"
+            primaryAction=""
+            destructiveAction="Delete permanently"
+        >
+            <EmailInput defaultValue="hello@exaple.com" />
+        </FormEntity>
+        <FormEntity
+            {...args}
+            title="Extra actions"
+            description="These extra actions are just small buttons"
+        >
+            <svelte:fragment slot="extra-actions">
+                <Button variant="secondary" size="small">Edit</Button>
+                <Button variant="secondary" size="small">Add</Button>
+            </svelte:fragment>
+            <EmailInput defaultValue="hello@exaple.com" />
+        </FormEntity>
+    </div>
+</Story>
+
+<Story name="With upload progress" let:args>
+    <FormEntity
+        {...args}
+        handleSubmit="{() => {
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve(true)
+                }, 500)
+            })
+        }}"
+        progress="{0.5}"
+    >
+        <EmailInput defaultValue="hello@exaple.com" />
+    </FormEntity>
+</Story>
---
+++ src/form/form-entity/form-entity.test.ts
@@ -0,0 +1,85 @@
+import { FORM_FEEDBACK } from '../form-feedback'
+
+const mockApiUrl = 'api'
+const errorCodes: [number, string][] = [
+    [400, FORM_FEEDBACK.errors[400]],
+    [403, FORM_FEEDBACK.errors[403]],
+    [429, FORM_FEEDBACK.errors[429]],
+    [500, FORM_FEEDBACK.errors[500]],
+]
+
+// eslint-disable-next-line max-lines-per-function -- tests are short
+context('FormEntity', () => {
+    it('renders', () => {
+        cy.loadStory('Form/FormEntity', 'Default')
+        cy.checkAccessibility()
+        cy.get('h3').contains('Form entity')
+        cy.get('button').contains('Save').click()
+        cy.get('button').should('be.disabled')
+        cy.contains(FORM_FEEDBACK.success.saved)
+        cy.get('button').should('not.be.disabled')
+
+        cy.loadStory('Form/FormEntity', 'With description')
+        cy.checkAccessibility()
+        cy.contains('Enter the email address')
+        cy.get('a').contains('Learn more').should('have.attr', 'href', '#')
+
+        cy.loadStory('Form/FormEntity', 'With upload progress')
+        cy.get('[role="progressbar"]').should('not.exist')
+        cy.get('button').contains('Save').click()
+        cy.checkAccessibility()
+        cy.get('[role="progressbar"]')
+            .should('have.attr', 'aria-valuemax', '1')
+            .should('have.attr', 'aria-label', 'Upload progress')
+        cy.contains(FORM_FEEDBACK.success.saved)
+        cy.get('[role="progressbar"]').should('not.exist')
+    })
+
+    it.only('can submit requests', () => {
+        cy.loadStory('Form/FormEntity', 'With description', {
+            method: 'POST',
+            action: mockApiUrl,
+        })
+        cy.intercept(mockApiUrl, { statusCode: 200 })
+        cy.get('button').click()
+        cy.get('[data-test="form-entity-success"]').contains(FORM_FEEDBACK.success.saved)
+
+        errorCodes.forEach(([code, message]) => {
+            cy.intercept(mockApiUrl, { statusCode: code })
+            cy.get('button').click()
+            cy.get('[data-test="form-entity-success"]').should('not.exist')
+            cy.get('[data-test="error"]').contains(message)
+        })
+
+        cy.goOffline()
+        cy.intercept(mockApiUrl, { statusCode: 200 })
+        cy.get('button').click()
+        cy.get('[data-test="form-entity-success"]').should('not.exist')
+        cy.get('[data-test="error"]').contains(FORM_FEEDBACK.errors.offline)
+        cy.goOnline()
+
+        cy.loadStory('Form/FormEntity', 'With description', {
+            method: 'POST',
+            action: mockApiUrl,
+            'error.default': 'default',
+            'error.400': '400',
+            'error.403': '403',
+            'error.429': '429',
+            'error.500': '500',
+            success: 'just success',
+        })
+
+        cy.intercept(mockApiUrl, { statusCode: 200 })
+        cy.get('button').click()
+        cy.get('[data-test="form-entity-success"]').contains('just success')
+
+        errorCodes.forEach(([code]) => {
+            cy.intercept(mockApiUrl, { statusCode: code })
+            cy.get('button').click()
+            cy.get('[data-test="form-entity-success"]').should('not.exist')
+            cy.get('[data-test="error"]').contains(code)
+        })
+    })
+})
+
+export {}
