f09aaf3fa1c97fc44908587e9304249872146a6b
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2021-05-23 11:57:40 -0700

    docs: add details to form-layout docs

--- src/form/form-layout/form-layout.stories.svelte
+++ src/form/form-layout/form-layout.stories.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
     import { Meta, Story } from '@storybook/addon-svelte-csf'
+    import Button from '../../elements/button/button.svelte'
+    import TextInput from '../inputs/text-input/text-input.svelte'
 
     import FormLayout from './form-layout.svelte'
 
@@ -9,7 +11,8 @@
         parameters: {
             docs: {
                 description: {
-                    component: '',
+                    component:
+                        'A wrapper component to provide consisten vertical spacing for forms, even if inputs have errors, feedback, or help text.',
                 },
             },
         },
@@ -21,5 +24,14 @@
 <Meta {...meta} />
 
 <Story name="Default" let:args>
-    <FormLayout {...args} />
+    <FormLayout {...args}>
+        <TextInput label="Input 1" />
+        <TextInput label="Input 2" helpText="This is some help text" />
+        <TextInput label="Input 3" maxCharacters="{32}" />
+        <TextInput label="Input 4" optional />
+        <TextInput label="Input 5" readonly />
+        <div class="flex justify-end">
+            <Button submit>Submit</Button>
+        </div>
+    </FormLayout>
 </Story>
