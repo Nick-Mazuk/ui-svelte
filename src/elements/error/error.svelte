d99d13897b42aa5f111b80c854889d00d7670a0f
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2021-05-23 10:49:03 -0700

    feat: add small error size

--- src/elements/error/error.svelte
+++ src/elements/error/error.svelte
@@ -2,11 +2,25 @@
     import AlertCircle from '../icon/alert-circle.svelte'
 
     type Action = { text: string; href: string } | undefined
-    type Size = 'default' | 'large'
+    type Size = 'small' | 'default' | 'large'
     export let action: Action = undefined
     export let label = 'Error'
     export let size: Size = 'default'
     export let dark = false
+
+    let iconSize: number
+    $: {
+        if (size === 'small') iconSize = 3.5
+        else if (size === 'default') iconSize = 4
+        else iconSize = 5
+    }
+    let iconStrokeWeight: number
+    $: {
+        if (size === 'small') iconStrokeWeight = 2.5
+        else if (size === 'default') iconStrokeWeight = 2
+        else iconStrokeWeight = 1.5
+    }
+
 </script>
 
 <div
@@ -15,15 +29,13 @@
     class="flex space-x-2 items-center"
     class:text-error="{!dark}"
     class:text-error-700="{dark}"
+    class:text-sm="{size === 'small'}"
     class:text-lg="{size === 'large'}"
     data-test="error"
 >
     <div class="flex items-center self-start flex-none">
         <span>&ZeroWidthSpace;</span>
-        <AlertCircle
-            size="{size === 'default' ? 4 : 5}"
-            strokeWidth="{size === 'default' ? 2 : 1.5}"
-        />
+        <AlertCircle size="{iconSize}" strokeWidth="{iconStrokeWeight}" />
     </div>
     <p>
         {#if label}
