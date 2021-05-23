69884ff80eb8679314088ba73cda1979d278147c
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2021-05-23 11:36:22 -0700

    refactor: remove duplicated code

--- src/form/form-layout/form-layout.svelte
+++ src/form/form-layout/form-layout.svelte
@@ -1,15 +1,10 @@
 <script lang="ts">
     import { setContext } from 'svelte'
 
-    import type { FormItemSize } from '../form-sizes'
-    import { INPUT_LAYOUT_SIZE_MAP } from '../form-sizes'
-
-    export let size: FormItemSize = 'default'
-
     setContext('formLayout', true)
 
 </script>
 
-<div class="grid grid-cols-1 {INPUT_LAYOUT_SIZE_MAP[size]}">
+<div class="grid grid-cols-1 gap-6">
     <slot />
 </div>
