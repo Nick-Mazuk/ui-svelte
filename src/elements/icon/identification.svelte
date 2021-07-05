47ae99b4eebe034720b74885ff7fec64ee394e03
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2021-07-04 21:10:33 -0700

    build icons

---
+++ src/elements/icon/identification.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    export let size = 8
+    export let strokeWidth = 1.5
+    export let color = ''
+    type Align = 'top' | 'middle' | 'bottom' | 'default'
+    export let align: Align = 'default'
+    export let title = ''
+    export let flexNone = false
+    export let fill = false
+    export let stroke = true
+
+    const ALIGN_MAP: Record<Align, string> = {
+        top: 'vertical-align: text-top;',
+        middle: 'vertical-align: middle;',
+        bottom: 'vertical-align: text-bottom;',
+        default: '',
+    }
+
+    $: dimensions = size * 4
+    $: parsedColor = color ? `rgb(var(${color}))` : 'currentColor'
+</script>
+
+<svg
+    viewBox="0 0 24 24"
+    width="{dimensions}"
+    height="{dimensions}"
+    stroke="{stroke ? 'currentColor' : 'none'}"
+    stroke-width="{strokeWidth}"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    fill="{fill ? 'currentColor' : 'none'}"
+    shape-rendering="geometricPrecision"
+    style="color: {parsedColor}; {ALIGN_MAP[align]}"
+    class:flex-none="{flexNone}"
+>
+    {#if title}
+        <title>{title}</title>
+    {/if}
+    <path d="M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2M15 11h3m-3 4h2"></path>
+</svg>
---
+++ src/elements/icon/office-building.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    export let size = 8
+    export let strokeWidth = 1.5
+    export let color = ''
+    type Align = 'top' | 'middle' | 'bottom' | 'default'
+    export let align: Align = 'default'
+    export let title = ''
+    export let flexNone = false
+    export let fill = false
+    export let stroke = true
+
+    const ALIGN_MAP: Record<Align, string> = {
+        top: 'vertical-align: text-top;',
+        middle: 'vertical-align: middle;',
+        bottom: 'vertical-align: text-bottom;',
+        default: '',
+    }
+
+    $: dimensions = size * 4
+    $: parsedColor = color ? `rgb(var(${color}))` : 'currentColor'
+</script>
+
+<svg
+    viewBox="0 0 24 24"
+    width="{dimensions}"
+    height="{dimensions}"
+    stroke="{stroke ? 'currentColor' : 'none'}"
+    stroke-width="{strokeWidth}"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    fill="{fill ? 'currentColor' : 'none'}"
+    shape-rendering="geometricPrecision"
+    style="color: {parsedColor}; {ALIGN_MAP[align]}"
+    class:flex-none="{flexNone}"
+>
+    {#if title}
+        <title>{title}</title>
+    {/if}
+    <path d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"></path>
+</svg>
