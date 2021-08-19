1fb20fa36a0806d7d4f124d3971f4c2f2f55dcd1
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2021-08-19 11:44:07 -0700

    fix: fix rendering issue caused by last update on changelog

--- src/layouts/changelog/changelog.svelte
+++ src/layouts/changelog/changelog.svelte
@@ -80,108 +80,113 @@
             </div>
         {/if}
         {#each items as item, index (item.publishedAt)}
-            <article
-                class="relative py-16 lg:ml-64"
-                class:border-t="{index !== 0 || $$slots.subscribe}"
-                style="content-visibility: auto; content-intrinsic-size: 1px 600px"
-            >
-                <div
-                    class="absolute bottom-0 border-l-2 -ml-20 lg:block"
-                    class:-top-px="{index !== 0 || $$slots.subscribe}"
-                    class:top-20="{index === 0 && !$$slots.subscribe}"
-                ></div>
-                <div
-                    class="absolute hidden lg:flex space-x-8 top-16 right-full w-64 justify-end items-center mr-16 transform translate-x-px"
+            <div style="content-visibility: auto; content-intrinsic-size: 1px 600px">
+                <article
+                    class="relative py-16 lg:ml-64"
+                    class:border-t="{index !== 0 || $$slots.subscribe}"
                 >
-                    <p class="text-lg text-gray">
-                        {#if typeof hrefPrefix === 'undefined'}
-                            {formatDate(item.publishedAt)}
-                        {:else}
-                            <a href="{createItemUrl(item.slug)}" title="View post {item.title}">
-                                {formatDate(item.publishedAt)}
-                            </a>
-                        {/if}
-                    </p>
-
                     <div
-                        class="relative w-8 h-8 flex items-center justify-center {VARIANT_MAP[
-                            item.variant
-                        ].class} rounded-full text-background dark:text-foreground"
+                        class="absolute bottom-0 border-l-2 -ml-20 lg:block"
+                        class:-top-px="{index !== 0 || $$slots.subscribe}"
+                        class:top-20="{index === 0 && !$$slots.subscribe}"
+                    ></div>
+                    <div
+                        class="absolute hidden lg:flex space-x-8 top-16 right-full w-64 justify-end items-center mr-16 transform translate-x-px"
                     >
-                        <svelte:component this="{VARIANT_MAP[item.variant].icon}" size="{5}" />
+                        <p class="text-lg text-gray">
+                            {#if typeof hrefPrefix === 'undefined'}
+                                {formatDate(item.publishedAt)}
+                            {:else}
+                                <a href="{createItemUrl(item.slug)}" title="View post {item.title}">
+                                    {formatDate(item.publishedAt)}
+                                </a>
+                            {/if}
+                        </p>
+
+                        <div
+                            class="relative w-8 h-8 flex items-center justify-center {VARIANT_MAP[
+                                item.variant
+                            ].class} rounded-full text-background dark:text-foreground"
+                        >
+                            <svelte:component this="{VARIANT_MAP[item.variant].icon}" size="{5}" />
+                        </div>
                     </div>
-                </div>
-                <div class="lg:hidden">
-                    <p class="text-lg text-gray">
+                    <div class="lg:hidden">
+                        <p class="text-lg text-gray">
+                            {#if typeof hrefPrefix === 'undefined'}
+                                {formatDate(item.publishedAt)}
+                            {:else}
+                                <a href="{createItemUrl(item.slug)}" title="View post {item.title}">
+                                    {formatDate(item.publishedAt)}
+                                </a>
+                            {/if}
+                        </p>
+                        <Spacer />
+                    </div>
+                    <div class="aspect-w-16 aspect-h-9 rounded-2xl shadow-lg overflow-hidden">
+                        <picture>
+                            {#if item.image.srcSet}
+                                {#each item.image.srcSet as source (source.src)}
+                                    <source
+                                        srcSet="{source.src}"
+                                        media="{source.media}"
+                                        type="{source.type}"
+                                        sizes="{source.sizes}"
+                                    />
+                                {/each}
+                            {/if}
+                            {#if item.image.src}
+                                <source srcSet="{item.image.src}" />
+                            {/if}
+                            <img
+                                alt=""
+                                class="object-cover w-full h-full"
+                                width="{item.image.width}"
+                                height="{item.image.height}"
+                                loading="{index === 0 ? 'eager' : 'lazy'}"
+                            />
+                        </picture>
+                    </div>
+                    <Spacer y="{2}" />
+                    <h2 class="h4">
                         {#if typeof hrefPrefix === 'undefined'}
-                            {formatDate(item.publishedAt)}
+                            {item.title}
                         {:else}
                             <a href="{createItemUrl(item.slug)}" title="View post {item.title}">
-                                {formatDate(item.publishedAt)}
+                                {item.title}
                             </a>
                         {/if}
-                    </p>
+                    </h2>
                     <Spacer />
-                </div>
-                <div class="aspect-w-16 aspect-h-9 rounded-2xl shadow-lg overflow-hidden">
-                    <picture>
-                        {#if item.image.srcSet}
-                            {#each item.image.srcSet as source (source.src)}
-                                <source
-                                    srcSet="{source.src}"
-                                    media="{source.media}"
-                                    type="{source.type}"
-                                    sizes="{source.sizes}"
-                                />
-                            {/each}
-                        {/if}
-                        {#if item.image.src}
-                            <source srcSet="{item.image.src}" />
-                        {/if}
-                        <img
-                            alt=""
-                            class="object-cover w-full h-full"
-                            width="{item.image.width}"
-                            height="{item.image.height}"
-                            loading="{index === 0 ? 'eager' : 'lazy'}"
-                        />
-                    </picture>
-                </div>
-                <Spacer y="{2}" />
-                <h2 class="h4">
-                    {#if typeof hrefPrefix === 'undefined'}
-                        {item.title}
-                    {:else}
-                        <a href="{createItemUrl(item.slug)}" title="View post {item.title}">
-                            {item.title}
-                        </a>
+                    <div class="prose">
+                        {@html item.contents}
+                    </div>
+                    {#if showReadMore && typeof hrefPrefix !== 'undefined'}
+                        <Spacer />
+                        <Button suffix="{ChevronRight}" href="{createItemUrl(item.slug)}">
+                            Read more
+                        </Button>
                     {/if}
-                </h2>
-                <Spacer />
-                <div class="prose">
-                    {@html item.contents}
-                </div>
-                {#if showReadMore && typeof hrefPrefix !== 'undefined'}
-                    <Spacer />
-                    <Button suffix="{ChevronRight}" href="{createItemUrl(item.slug)}">
-                        Read more
-                    </Button>
-                {/if}
-            </article>
+                </article>
+            </div>
         {/each}
         {#if showLoadMore || showReachedEnd}
-            <div class="relative py-16 border-t lg:ml-64">
-                <div class="absolute -top-px bottom-0 border-l-2 -ml-20 lg:block"></div>
-                <div class="flex justify-center">
-                    {#if showLoadMore}
-                        <Button
-                            variant="secondary"
-                            loading="{isLoading}"
-                            on:click="{() => dispatch('loadMore')}">Load more</Button
-                        >
-                    {:else}
-                        <p class="text-gray-700">You've reached the end</p>
-                    {/if}
+            <div style="content-visibility: auto; content-intrinsic-size: 1px 168px">
+                <div class="relative py-16 border-t lg:ml-64">
+                    <div class="absolute -top-px bottom-0 border-l-2 -ml-20 lg:block"></div>
+                    <div class="flex justify-center">
+                        {#if showReachedEnd}
+                            <p class="text-gray-700">You've reached the end</p>
+                        {:else}
+                            <Button
+                                variant="secondary"
+                                loading="{isLoading}"
+                                on:click="{() => dispatch('loadMore')}"
+                            >
+                                Load more
+                            </Button>
+                        {/if}
+                    </div>
                 </div>
             </div>
         {/if}
